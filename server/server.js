a27ae538b7da1d03a73676b47e28a86b66db29b3
Abdul-Faizal-05 <abdulfaizal18042006@gmail.com>
2025-06-23 10:17:44 +0530

    Database connected successfully

--- server/server.js
+++ server/server.js
@@ -1,12 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const {Pool} = require('pg')
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+const database = new Pool({
+    user: process.env.db_username,
+    host: process.env.db_host,
+    database: process.env.db_database,
+    password: process.env.db_password,
+    port: process.env.db_port
+});
 
 app.post('/register',async(req,res)=>{
     const {name,dob,email,password} = req.body;
@@ -14,6 +22,14 @@ app.post('/register',async(req,res)=>{
     res.status(200).json({message:"Data received successfully"})
 })
 
-app.listen(5000,()=>{
-    console.log('Server was successfully running on 5000');
-})
+database.connect()
+  .then(() => {
+    console.log('Database connected successfully');
+    app.listen(5000, () => {
+      console.log('Server is running successfully on port 5000');
+    });
+  })
+  .catch(err => {
+    console.error('Database connection error:', err);
+    process.exit(1);
+  });
